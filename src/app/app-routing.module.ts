108c295a070e2ea673de12e2bdf2e5815f493345
damarjatiSK <dmrjtsk363@gmail.com>
2022-12-06 18:31:58 +0700

    responsi kedua damarjati

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,51 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { AutoLoginGuard } from './guards/auto-login.guard';
+import { AuthGuard } from './guards/auth.guard';
 
 const routes: Routes = [
-  {
-    path: '',
-    redirectTo: 'folder/Inbox',
-    pathMatch: 'full'
-  },
+  // {
+  //   path: 'home',
+  //   loadChildren: () => import('./home/home.module').then(m => m.HomePageModule),
+  //   canLoad: [AuthGuard] // Secure all child pages
+  // },
+  // {
+  //   path: '',
+  //   loadChildren:() => import('./mahasiswa/mahasiswa.module').then(m=> m.MahasiswaPageModule),
+  //   pathMatch: 'full'
+  // },
   {
     path: 'folder/:id',
     loadChildren: () => import('./folder/folder.module').then( m => m.FolderPageModule)
-  }
+  },
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full'
+  },
+  {
+    path: 'login',
+    loadChildren: () => import('./login/login.module').then(m =>
+    m.LoginPageModule),
+    canLoad: [AutoLoginGuard]
+  },
+  {
+    path: 'mahasiswa',
+    loadChildren: () => import('./mahasiswa/mahasiswa.module').then( m => m.MahasiswaPageModule),
+    canLoad: [AuthGuard],
+  },
+  {
+    path: 'mahasiswa-tambah',
+    loadChildren: () => import('./mahasiswa-tambah/mahasiswa-tambah.module').then( m => m.MahasiswaTambahPageModule)
+  },
+  {
+    path: 'mahasiswa-edit/:nim',
+    loadChildren: () => import('./mahasiswa-edit/mahasiswa-edit.module').then( m => m.MahasiswaEditPageModule)
+  },
+  {
+    path: 'home',
+    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+  },
 ];
 
 @NgModule({
